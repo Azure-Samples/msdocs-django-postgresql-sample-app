1e23bb10cdba768e88a02bd7da08cc9301a22f4d
vmagelo <51368918+vmagelo@users.noreply.github.com>
2022-02-23 18:21:22 +0100

    Parameters to work in production.

--- azureproject/production.py
+++ azureproject/production.py
@@ -19,7 +19,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # DBHOST is only the server name, not the full URL
 hostname = os.environ['DBHOST']
