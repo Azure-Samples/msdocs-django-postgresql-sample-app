cd89f0a6dad076baf35bac48a655ea4294910df0
vmagelo <51368918+vmagelo@users.noreply.github.com>
2022-02-23 20:35:18 +0100

    Should show best practice for production.

--- azureproject/production.py
+++ azureproject/production.py
@@ -4,6 +4,7 @@ import os
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+DEBUG = False
 
 # WhiteNoise configuration
 MIDDLEWARE = [                                                                   
