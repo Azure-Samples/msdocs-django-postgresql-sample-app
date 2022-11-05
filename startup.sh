2e6f8174298cba97eaa5c2b61531be2d742bd79d
Pamela Fox <pamelafox@microsoft.com>
2022-11-05 23:22:54 +0000

    Startup script fix

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
 gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
     '-' --error-logfile '-' --bind=0.0.0.0:8000 \
-     --chdir=/home/site/wwwroot project.wsgi -c gunicorn.conf.py
+     --chdir=/home/site/wwwroot azureproject.wsgi
