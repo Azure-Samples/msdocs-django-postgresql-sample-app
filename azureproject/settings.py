4f25f061b5a7f860137af50e8c4ea4133f3f2d25
Pamela Fox <pamelafox@microsoft.com>
2022-11-16 21:48:21 +0000

    CSRF settings

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+if 'CODESPACE_NAME' in os.environ:
+    CSRF_TRUSTED_ORIGINS = [f'https://{os.getenv("CODESPACE_NAME")}-8000.{os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")}']
 
 # Application definition
 
