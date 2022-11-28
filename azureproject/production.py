eda7705d8c74a096df94c8ce347c23da94f27eb5
WalidElAoun <119173565+WalidElAoun@users.noreply.github.com>
2022-11-28 18:31:09 +0100

    Update production.py

--- azureproject/production.py
+++ azureproject/production.py
@@ -3,8 +3,8 @@ import os
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = [os.environ['universityapplication-server.postgres.database.azure.com']] if 'universityapplication-server.postgres.database.azure.com' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['universityapplication-server.postgres.database.azure.com']] if 'universityapplication-server.postgres.database.azure.com' in os.environ else []
 DEBUG = False
 
 # WhiteNoise configuration
@@ -24,16 +24,16 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # DBHOST is only the server name, not the full URL
-hostname = os.environ['DBHOST']
+hostname = os.environ['universityapplication-server']
 
 # Configure Postgres database; the full username for PostgreSQL flexible server is
 # username (not @sever-name).
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DBNAME'],
+        'NAME': os.environ['postgres'],
         'HOST': hostname + ".postgres.database.azure.com",
-        'USER': os.environ['DBUSER'],
-        'PASSWORD': os.environ['DBPASS'] 
+        'USER': os.environ['otpljolaqm'],
+        'PASSWORD': os.environ['2HO2E0P83B7T3NZ8$'] 
     }
 }
