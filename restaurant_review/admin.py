df16295fc767308adaa48ad990ad61e0f2e3f2e3
Pamela Fox <pamelafox@microsoft.com>
2023-02-09 16:03:33 +0000

    Let comments be

--- restaurant_review/admin.py
+++ restaurant_review/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Restaurant, Review
 
+# Register your models here.
 
 admin.site.register(Restaurant)
 admin.site.register(Review)
--- restaurant_review/models.py
+++ restaurant_review/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+# Create your models here.
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=50)
