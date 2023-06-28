c20e441baca61a401c99c864a29b427622e27923
Antonio Perez <antoniopereztobon@gmail.com>
2023-06-28 11:20:22 -0500

    Poblar personas y equipos

---
+++ populate_equipo.py
@@ -0,0 +1,40 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "azureproject.settings")
+
+import django
+
+django.setup()
+
+import csv
+from ingreso.models import Equipo
+
+
+def populate(N):
+    with open("equipo.csv", encoding="utf-8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=";")
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Columnas: {", ".join(row)}')
+                line_count += 1
+            else:
+                eq_tipo = row[0]
+                eq_marca = row[1]
+                eq_serie = row[2]
+                equipo = Equipo.objects.get_or_create(
+                    eq_tipo=eq_tipo, eq_marca=eq_marca, eq_serie=eq_serie
+                )[0]
+
+                line_count += 1
+
+            if line_count > N:
+                break
+
+        print(f"Processed {line_count} lines.")
+
+
+if __name__ == "__main__":
+    print("populating script!")
+    populate(5000)
+    print("populating complete!")
---
+++ populate_persona.py
@@ -0,0 +1,40 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "azureproject.settings")
+
+import django
+
+django.setup()
+
+import csv
+from ingreso.models import Persona
+
+
+def populate(N):
+    with open("persona.csv", encoding="utf-8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=";")
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Columnas: {", ".join(row)}')
+                line_count += 1
+            else:
+                id_tipo = "CC"
+                id_num = row[0]
+                nom_persona = row[1]
+                persona = Persona.objects.get_or_create(
+                    id_tipo=id_tipo, id_num=id_num, nom_persona=nom_persona
+                )[0]
+
+                line_count += 1
+
+            if line_count > N:
+                break
+
+        print(f"Processed {line_count} lines.")
+
+
+if __name__ == "__main__":
+    print("populating script!")
+    populate(5000)
+    print("populating complete!")
