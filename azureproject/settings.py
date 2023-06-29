9c719eeb49a5715da06d5d9e514bf943991f2c27
Antonio Perez <antoniopereztobon@gmail.com>
2023-06-29 10:40:01 -0500

    Ult cambios iniciales

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -151,7 +151,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = "/ing/user_login"
+#LOGIN_URL = "/ingreso/user_login"
 
 # Other settings...
 
--- azureproject/urls.py
+++ azureproject/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from ingreso import views
+#from ingreso import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ingreso.urls')),
-    path("", views.IndexView.as_view(), name="index"),
-    path("logout/", views.user_logout, name="logout"),
+    #path('', include('ingreso.urls')),
+    #path("", views.IndexView.as_view(), name="index"),
+    #path("logout/", views.user_logout, name="logout"),
 ]
