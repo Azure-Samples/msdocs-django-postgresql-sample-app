a806eca777c3ca4d73d197ce0c8e8acaa0c14d6a
Antonio Perez <antoniopereztobon@gmail.com>
2023-06-29 11:23:47 -0500

    De nuevo retirar bootstrap5

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "bootstrap5",
+    #"bootstrap5",
     "crispy_forms",
     "crispy_bootstrap5",
     "ingreso",
