97a4a74fef24373984645f6d8301c15d377a1aea
Miles Eu <mileseu1@gmail.com>
2023-10-23 03:10:09 +0100

    add host

--- azureproject/production.py
+++ azureproject/production.py
@@ -5,7 +5,7 @@ from .settings import BASE_DIR
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], 'milesforsarah.cubetown.co.uk'] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 DEBUG = False
 
