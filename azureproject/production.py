2638cdb214b877ff5b86873a49b16b44b5f360ca
Cephas Lin <cephalin@users.noreply.github.com>
2023-11-28 16:45:25 +0100

    revert to Django db config

--- azureproject/production.py
+++ azureproject/production.py
@@ -31,12 +31,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Uncomment the following lines for App Service
 # conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 # conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
-# DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-#     dbuser=conn_str_params['user'],
-#     dbpass=conn_str_params['password'],
-#     dbhost=conn_str_params['host'],
-#     dbname=conn_str_params['dbname']
-# )
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': conn_str_params['dbname'],
+#         'HOST': conn_str_params['host'],
+#         'USER': conn_str_params['user'],
+#         'PASSWORD': conn_str_params['password'],
+#     }
+# }
 
 # CACHES = {
 #         "default": {  
